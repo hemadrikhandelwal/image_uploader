1f66465799e087927e7533112c72344b4354e0dd
hemadrikhandelwal <hemadrikhandelwal@gmail.com>
2026-05-13 11:03:27 +0530

    folder restructure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { ImageUploader } from './features/images/image-uploader/image-uploader';
-import { ImageGallery } from './features/images/image-gallery/image-gallery';
+import { ImageUploader } from './features/images/components/image-uploader/image-uploader';
+import { ImageGallery } from './features/images/components/image-gallery/image-gallery';
 
 export const routes: Routes = [
     {
